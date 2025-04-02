f0ece26a37f744afd4287fa56bb308b7ba728bb5
Akshara266 <sanagalaakshara@gmail.com>
2025-04-02 13:31:34 -0500

    added files

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var cRouter = require('./routes/Gadgets');
+var GadgetsRouter = require('./routes/Gadgets');
 
 var app = express();
 
